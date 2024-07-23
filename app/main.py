f64d1d6e8b463437f094c0d0b795d4baadf8be22
SebastianKuleshov <sebaseba7775@gmail.com>
2024-07-23 14:08:27 +0300

    fix: added and configured CORSMiddleware and added docker, docker-compose

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.users.routes import router as users_router
 from app.auth.routes import router as auth_router
 from app.comments.routes import router as comments_router
@@ -6,6 +8,14 @@ from app.answers.routes import router as answers_router
 from app.questions.routes import router as questions_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 app.include_router(users_router)
 app.include_router(auth_router)
