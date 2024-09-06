2de0d43d910db6219fa278a8165734114c0d12e2
SebastianKuleshov <sebaseba7775@gmail.com>
2024-09-06 12:31:41 +0300

    added .env variables

--- replace-placeholders.sh
+++ replace-placeholders.sh
@@ -8,7 +8,10 @@ mkdir -p /opt/keycloak/data/import
 
 echo "Replacing placeholders in realm configuration file..."
 
+: ${ACCESS_TOKEN_EXPIRE_MINUTES:=5}
+
 SUPERUSER_CREATED_TIMESTAMP=$(date +%s%3N)
+ACCESS_TOKEN_LIFESPAN=$((ACCESS_TOKEN_EXPIRE_MINUTES * 60))
 
 # Replace placeholders with environment variables
 sed -e "s|__SUPERUSER_EMAIL__|${SUPERUSER_EMAIL}|g" \
@@ -17,6 +20,7 @@ sed -e "s|__SUPERUSER_EMAIL__|${SUPERUSER_EMAIL}|g" \
     -e "s|__EMAIL_SMTP_PORT__|${EMAIL_SMTP_PORT}|g" \
     -e "s|__EMAIL_SMTP_USERNAME__|${EMAIL_SMTP_USERNAME}|g" \
     -e "s|__EMAIL_SMTP_PASSWORD__|${EMAIL_SMTP_PASSWORD}|g" \
+    -e "s|__ACCESS_TOKEN_LIFESPAN__|${ACCESS_TOKEN_LIFESPAN}|g" \
     $TEMPLATE_FILE > $REALM_FILE
 
 
