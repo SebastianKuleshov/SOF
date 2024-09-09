2da45583a7d918e801d57028fe4f24fe3dd12f3a
SebastianKuleshov <sebaseba7775@gmail.com>
2024-09-09 10:41:07 +0300

    moved models import

--- app/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.answers.models import AnswerModel
-from app.comments.models import CommentModel
-from app.questions.models import QuestionModel
-from app.tags.models import TagModel
-from app.users.models import UserModel
-from app.votes.models import VotesModel
